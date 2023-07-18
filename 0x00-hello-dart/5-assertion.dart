fdd4d43a5344c4702976c539212b78db23039484
jaafar <jaafarferes@gmail.com>
2023-07-18 10:51:49 +0100

    dart assert condition

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,16 +1,7 @@
+// dart assert condition
 // in order to make it check if the number is bigger or equal to 80 print You Passed otherwise the output should be "..."
 
 void main(List<String> args) {
   int a = int.parse(args[0]);
-  switch(a) { 
-   case <= 79: { 
-      print('Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
-   } 
-   break; 
-  
-   default: { 
-      print('You Passed');
-   } 
-   break; 
-}
+   (a >= 80) ? print('You Passed') : throw ('The score must be bigger or equal to 80');
 }
