b1fbce11fbc9f3dad69d359ca1a1c12f68f2f30a
jaafar <jaafarferes@gmail.com>
2023-07-19 15:33:07 +0100

    4. Outer Inner

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,9 +1,9 @@
+// outer function 
 void outer(String name, String id) {
   String inner() {
-  List<String> nameParts = name.split(" ");
-  String firstNameInitial = nameParts[0][0].toUpperCase();
-  String lastName = nameParts[1];
-  return "Hello Agent $firstNameInitial.$lastName your id is $id";
+  var nameParts = name.split(" ");
+  String firstNameInitial = nameParts[1].substring(0, 1) + "." + nameParts[0];
+  return "Hello Agent $firstNameInitial your id is $id";
 }
 print(inner());
 }
