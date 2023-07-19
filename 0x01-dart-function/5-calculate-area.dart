147e54329895124743de55f99c8081cf0bf428f8
jaafar <jaafarferes@gmail.com>
2023-07-19 15:50:35 +0100

    5. Calculate Area  of rectangle using height and base

---
+++ 0x01-dart-function/5-calculate-area.dart
@@ -0,0 +1,6 @@
+//calculate the rectangle area using the height and base 
+double calculateArea(double height, double base) {
+  double i = 0.5 * height * base;
+  i = double.parse(i.toStringAsFixed(2));
+  return i;
+}
