d428fd2fc901dd608277b575833778f7a38886a4
jaafar <jaafarferes@gmail.com>
2023-07-19 17:00:13 +0100

    calculate the winner of a basketball game

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,10 +1,16 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  var teamAScore =
-      teamA['Free throws']! + teamA['2 pointer']! * 2 + teamA['3 pointer']! * 3;
-  var teamBScore =
-      teamB['Free throws']! + teamB['2 pointer']! * 2 + teamB['3 pointer']! * 3;
+  var teamAScore = teamA['Free throws']! +
+      teamA['2 pointers']! * 2 +
+      teamA['3 pointers']! * 3;
+  var teamBScore = teamB['Free throws']! +
+      teamB['2 pointers']! * 2 +
+      teamB['3 pointers']! * 3;
 
-  if (teamAScore > teamBScore) return 1;
-  if (teamAScore < teamBScore) return 2;
-  return 0;
+  if (teamAScore > teamBScore) {
+    return 1;
+  } else if (teamAScore < teamBScore) {
+    return 2;
+  } else {
+    return 0;
+  }
 }
