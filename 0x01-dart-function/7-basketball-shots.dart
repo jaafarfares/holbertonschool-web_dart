285f171ffdcb7789b65050a8643aa6f5a527dc35
jaafar <jaafarferes@gmail.com>
2023-07-19 16:42:36 +0100

    calculate the winner of a basketball game

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,25 +1,12 @@
-//calculate the winner of a basketball game
-int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  bool isKeyMissing = teamA.keys.toSet().union(teamB.keys.toSet()).length !=
-      teamA.keys.toSet().intersection(teamB.keys.toSet()).length;
-
-  if (isKeyMissing) {
-    return -1;
-  }
-
-  int teamAScore = (teamA['Free throws'] ?? 0) * 1 +
-      (teamA['2 pointer'] ?? 0) * 2 +
-      (teamA['3 pointer'] ?? 0) * 3;
-
-  int teamBScore = (teamB['Free throws'] ?? 0) * 1 +
-      (teamB['2 pointer'] ?? 0) * 2 +
-      (teamB['3 pointer'] ?? 0) * 3;
+int whoWins(Map<String, int> teamA, Map<String, int> teamB){
+  var teamAScore = teamA['Free throws']! + teamA['2 pointer']! * 2 + teamA['3 pointer']! * 3;
+  var teamBScore = teamB['Free throws']! + teamB['2 pointer']! * 2 + teamB['3 pointer']! * 3;
 
   if (teamAScore > teamBScore) {
-    return 1; // Team A wins
+    return 1;
   } else if (teamAScore < teamBScore) {
-    return 2; // Team B wins
+    return 2;
   } else {
-    return 0; // It's a tie
+    return 0;
   }
 }
