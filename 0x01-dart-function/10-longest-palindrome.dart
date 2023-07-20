992eac11044ce39ab8978a401a6a763d7a4778c0
Jaafar <98318912+jaafarfares@users.noreply.github.com>
2023-07-20 22:06:23 +0100

    Update 10-longest-palindrome.dart

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,23 +1,23 @@
 bool isPalindrome(String s) {
-
   if (s.length < 3) {
     return false;
   }
 
-
   String cleanedString = s.replaceAll(RegExp(r'[^a-zA-Z0-9]'), '').toLowerCase();
-
-
   return cleanedString == cleanedString.split('').reversed.join('');
 }
 
 String longestPalindrome(String s) {
-  String longest = "none";
+  if (s.length < 3) {
+    return null;
+  }
+
+  String longest = null;
 
   for (int i = 0; i < s.length; i++) {
     for (int j = i + 1; j <= s.length; j++) {
       String substring = s.substring(i, j);
-      if (isPalindrome(substring) && substring.length > longest.length) {
+      if (isPalindrome(substring) && (longest == null || substring.length > longest.length)) {
         longest = substring;
       }
     }
