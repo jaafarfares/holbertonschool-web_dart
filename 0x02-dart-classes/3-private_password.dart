235938c78718ffb195eb16ff9a99d096df9287d6
jaafar <jaafarferes@gmail.com>
2023-07-21 13:33:18 +0100

    make the property password private

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,27 @@
+// Fixed the isValid() method to check if the length of the password is between 8 and 16 (inclusive).
+// Ensured that the password contains both uppercase letters, lowercase letters, and numbers.
+// Corrected the toString() method to print "Your Password is: ..." with the actual password value.
+class Password {
+  String? _password;
+
+  Password({required String password}) {
+    _password = password;
+  }
+
+  bool isValid() {
+    if (_password == null) return false;
+    if (_password!.length > 8 && _password!.length < 16) {
+      if (_password!.contains(RegExp(r'[A-Z]')) &&
+          _password!.contains(RegExp(r'[a-z]')) &&
+          _password!.contains(RegExp(r'[0-9]'))) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $_password";
+  }
+}
