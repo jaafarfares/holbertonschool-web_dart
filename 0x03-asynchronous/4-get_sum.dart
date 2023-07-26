9c6432a370b29aabf993f9988b0f7a20954a0d10
jaafar <jaafarferes@gmail.com>
2023-07-26 12:48:41 +0100

    Create a function calculateTotal() that calculates the total price of items for a certain user.

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,1 +1,19 @@
+import 'dart:convert';
 
+import '4-util.dart';
+
+calculateTotal() async {
+  try {
+    double total = 0;
+
+    final Map<String, dynamic> userData = json.decode(await fetchUserData());
+    final String data = userData['id'];
+    final List<dynamic> userOrder = json.decode(await fetchUserOrders(data));
+    for (int i = 0; i < userOrder.length; i++) {
+      total += json.decode(await fetchProductPrice(userOrder[i]));
+    }
+    return total;
+  } catch (e) {
+    return -1;
+  }
+}
